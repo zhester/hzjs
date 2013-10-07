28fdac28d966c55a959d9ab8598c6d7187ee96bd
Zac Hester <zac.hester@gmail.com>
2013-10-06 20:47:48 -0500

    Adding uploader.js proof-of-concept.

---
+++ development/uploader.js
@@ -0,0 +1,10 @@
+/*****************************************************************************
+    Chunked, High-Volume File Uploader
+*****************************************************************************/
+
+function uploader( input_element ) {
+    input_element.onchange = function( e ) {
+        alert( 'onchange' );
+    }
+}
+
