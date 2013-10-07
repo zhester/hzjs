3613b60d2e5a1030dce6ca6bc2c32a5136121786
Zac Hester <zac.hester@gmail.com>
2013-10-07 16:29:33 -0500

    Nearly complete uploader system with example code.

--- development/uploader.js
+++ development/uploader.js
@@ -1,51 +1,371 @@
 /*****************************************************************************
     Chunked, High-Volume File Uploader
+
+    Note: This system requires server-side feature support.  The server must
+    support a simple POST interface that allows files to be uploaded in parts
+    and reassembled on the server's file system.  The protocol is as follows:
+
+        The client submits a POST request to the following URI (separated by
+        lines for readability):
+
+            <target>?name=<file_name>
+                &time_ms=<file_mtime(ms)>
+                &size=<file_size>
+                &offset=<current_offset>
+                &length=<current_length>
+
+        The body of the POST request contains the raw binary data of the part
+        of the file being uploaded (Content-Type: application/octet-stream).
+        These parts are iteratively submitted to the server until the entire
+        file has been sent.
+
+        Upon successful receipt of a file part, the server must respond to the
+        POST request with a simple JSON (Content-Type: application/json)
+        message:
+
+            {
+                "name"    : "<file_name>",
+                "time_ms" : <file_mtime(ms)>,
+                "size"    : <file_size>,
+                "offset"  : <current_offset>,
+                "length"  : <current_length>
+            }
+
+        This allows the client script to verify everything was received before
+        sending the next file part.  It's up to the server to decide how/when
+        to reassemble the uploaded file.
+
+        The maximum size of each file part can be set in the upload object.
+        The property is named "chunk_size".
 *****************************************************************************/
 
-function uploader( input_element ) {
-    this.input = input_element;
-    this.current_file = 0;
-    var context = this;
-    this.input.onchange = function( e ) {
-        context.input.disable = true;
-        if( context.input.files.length > 0 ) {
-            context.start_upload( 0 );
-        }
+
+/**
+ *  Multiple file upload manager.  Allows user code to upload multiple files
+ *  in one large process with little supervision.  This should also be used as
+ *  the reference implementation for using the chunked upload system.
+ *
+ *  Example usage:
+ *
+ *      var file_input = document.getElementById( 'file_input' );
+ *      var up = new uploader( file_input.files );
+ *      up.onload = function( e ) { alert( 'All files uploaded!' ); };
+ *      up.upload_all();
+ *
+ *  @param file_list    A FileList instance
+ *  @param target       The base URL to the upload server handler
+ */
+function uploader() {
+
+    //initialize the uploader object
+    this.current    = -1;
+    this.file_list  = null;
+    this.onabort    = null;
+    this.onerror    = null;
+    this.onload     = null;
+    this.onprogress = null;
+    this.ontimeout  = null;
+    this.target     = null;
+
+    //overall transfer status tracking
+    this.status = {
+        files_total: 0,
+        bytes_total: 0,
+        files_sent:  0,
+        bytes_sent:  0
     };
+
+    //check for a specified file list
+    if( arguments.length > 0 ) {
+        this.file_list = arguments[ 0 ];
+        this.status.files_total = this.file_list.length;
+        for( var i = 0; i < this.status.files_total; ++i ) {
+            this.status.bytes_total += this.file_list[ i ].size;
+        }
+    }
+
+    //check for a specified target URL
+    if( arguments.length > 1 ) {
+        this.target = arguments[ 1 ];
+    }
 }
 
-uploader.prototype.start_upload = function( index ) {
-    var flist = context.input.files;
-    var uppy = new upload( flist[ index ] );
-    // uppy.onload = function( e ) { #### }
-    uppy.send_chunk();
+
+/**
+ *  Initiates an upload of all files known to the uploader object.
+ *
+ */
+uploader.prototype.upload_all = function() {
+
+    //reset the current file index
+    this.current = -1;
+
+    //initialize the multi-file upload status
+    this.status.files_total = this.file_list.length;
+    this.status.bytes_total = 0;
+    for( var i = 0; i < this.status.files_total; ++i ) {
+        this.status.bytes_total += this.file_list[ i ].size;
+    }
+    this.status.files_sent = 0;
+    this.status.bytes_sent = 0;
+
+    //upload the next (first) file in the list
+    this.upload_next();
+
 };
 
 
+/**
+ *  Initiates an upload of the next file in the list.
+ *
+ */
+uploader.prototype.upload_next = function() {
 
-function upload( file ) {
-    this.file = file;
-    this.chunk_size = 10 * 1024 * 1024;
-    this.bytes_sent = 0;
-    this.reader = new FileReader();
-    this.xhr = new XMLHttpRequest();
-    this.onload = null;
-    var context = this;
-    this.reader.onload = function( e ) {
-        // set up a POST request using context.xhr
-        // post e.target.result
-    };
+    //increment the current file index
+    this.current += 1;
+
+    //bounds check the current file index
+    if( this.current < this.file_list.length ) {
+
+        //create the upload object for the current file
+        var up = this.create_upload( this.file_list[ this.current ] );
+
+        //set an object context variable for closed event handlers
+        var context = this;
+
+        //pass-through event handlers
+        up.onabort   = this.onabort;
+        up.onerror   = this.onerror;
+        up.ontimeout = this.ontimeout;
+
+        //handle the completion of this upload
+        up.onload = function( e ) {
+
+            //update status
+            context.status.files_sent += 1;
+            context.status.bytes_sent += e.loaded;
+
+            //attempt to upload the next file in the list (if there is one)
+            context.upload_next();
+        };
+
+        //handle progress updates during the upload
+        up.onprogress = function( e ) {
+            if( context.onprogress != null ) {
+                context.onprogress( {
+                    lengthComputable: true,
+                    loaded:           ( e.loaded + context.status.bytes_sent ),
+                    total:            context.status.bytes_total
+                } );
+            }
+        };
+
+        //initiate the chunked upload for this file
+        up.send();
+    }
+
+    //multi-file upload completed, and there's an onload handler
+    else if( this.onload != null ) {
+        this.onload( {
+            target:           this,
+            lengthComputable: true,
+            loaded:           this.status.bytes_sent,
+            total:            this.status.bytes_total
+        } );
+    }
+
 }
 
-upload.prototype.send_chunk = function() {
-    var left = this.file.size - this.bytes_sent;
+
+/**
+ *  Creates an upload object for the specified file instance.
+ *
+ *  @param file         A File instance to upload
+ *  @param target       The base URL to the upload server handler (optional)
+ *  @return             An upload object instance that can upload the file,
+ *                      or null on failure
+ */
+uploader.prototype.create_upload = function( file ) {
+
+    //initialize the default target URL
+    var target = this.target;
+
+    //check for a per-upload target URL
+    if( arguments.length > 1 ) {
+        target = arguments[ 1 ];
+    }
+
+    //create the upload object and return it
+    return new upload( file, target );
+};
+
+
+/**
+ *  Establishes a chunked file upload for a single file.  The file must be an
+ *  instance of a File object.  The user can subscribe to the four typical
+ *  XHR events (abort, error, load, progress) using property-style assignment.
+ *
+ *  @param file         The File instance representing the file to send
+ *  @param target       The target URL that can handle chunked uploads
+ */
+
+function upload( file, target ) {
+
+    //initialize the upload object
+    this.chunk_size  = 1 * 1024 * 1024;
+    this.file        = file;
+    this.onabort     = null;
+    this.onerror     = null;
+    this.onload      = null;
+    this.onprogress  = null;
+    this.ontimeout   = null;
+    this.reader      = new FileReader();
+    this.to_send     = 0;
+    this.target      = target;
+    this.xhr         = new XMLHttpRequest();
+    this.xhr.timeout = 5 * 60 * 1000;
+
+    //transfer status tracking
+    this.status = {
+        bytes_total: file.size,
+        bytes_sent:  0
+    };
+
+    //set an object context variable for closed event handlers
+    var context = this;
+
+    //pass-through event handlers
+    this.xhr.onabort = function( e ) {
+        if( context.onabort != null ) {
+            context.onabort( e );
+        }
+    };
+    this.xhr.onerror = function( e ) {
+        if( context.onerror != null ) {
+            context.onerror( e );
+        }
+    };
+    this.xhr.ontimeout = function( e ) {
+        if( context.ontimeout != null ) {
+            context.ontimeout( e );
+        }
+    }
+
+    //progress is re-mapped to the overall upload (not just a chunk)
+    this.xhr.onprogress = function( e ) {
+        if( context.onprogress != null ) {
+            context.onprogress( {
+                lengthComputable: true,
+                loaded:           ( e.loaded + context.status.bytes_sent ),
+                total:            context.file.size
+            } );
+        }
+    };
+
+    //load is re-mapped to the overall upload (not just a chunk)
+    this.xhr.onload = function( e ) {
+
+        //parse the server's response
+        var result = JSON.parse( context.xhr.responseText );
+
+        //check the transfer for any problems
+        if( result.length != context.to_send ) {
+
+            //if the error handler is available...
+            if( context.onerror != null ) {
+
+                //indicate an error with some custom fields
+                context.onerror( {
+                    lengthComputable: false,
+                    loaded:           0,
+                    total:            0,
+                    message:          'Server reports incorrect size.',
+                    bytes_sent:       context.to_send,
+                    bytes_received:   result.length
+                } );
+            }
+        }
+
+        //the transfer appears successful
+        else {
+
+            //increment the total bytes sent
+            context.status.bytes_sent += context.to_send;
+
+            //send the next chunk
+            context.send();
+        }
+    };
+
+    //the file reader load event signals when it's ready to upload
+    this.reader.onload = function( e ) {
+
+        //construct the URI for the POST request
+        var uri = context.target
+            + '?name='    + encodeURIComponent( context.file.name )
+            + '&time_ms=' + context.file.lastModifiedDate.getTime()
+            + '&size='    + context.status.bytes_total
+            + '&offset='  + context.status.bytes_sent
+            + '&length='  + e.target.result.length;
+
+        //prepare the proper POST request
+        context.xhr.open( 'POST', uri, true );
+        context.xhr.setRequestHeader(
+            'Content-Type',
+            'application/octet-stream'
+        );
+        context.xhr.setRequestHeader(
+            'Content-Length',
+            e.target.result.length
+        );
+        context.xhr.setRequestHeader( 'Connection', 'close' );
+
+        //send the POST request
+        //ZIH - FF only, will need to look into Chrome support
+        context.xhr.sendAsBinary( e.target.result );
+    };
+
+}
+
+
+/**
+ *  Sends the next part of the file to the server.
+ *
+ */
+upload.prototype.send = function() {
+
+    //determine the number of remaining bytes to send
+    var left = this.status.bytes_total - this.status.bytes_sent;
+
+    //check if there are bytes left to send
     if( left > 0 ) {
-        var send = left > this.chunk_size ? this.chunk_size : left;
-        this.reader.readAsDataURL(
-            this.file.slice( this.bytes_sent, ( this.bytes_sent + send ) )
+
+        //calculate the bytes to send in this transfer
+        this.to_send = left > this.chunk_size ? this.chunk_size : left;
+
+        //initiate a file read for the number of bytes to send
+        //ZIH - this is used to complement xhr.sendAsBinary
+        this.reader.readAsBinaryString(
+
+            //specify the part of the file that should be sent
+            //ZIH - Chrome may not support file.slice()
+            this.file.slice(
+                this.status.bytes_sent,
+                ( this.status.bytes_sent + this.to_send )
+            )
         );
     }
+
+    //there are no more bytes left to send, check for load handler
     else if( this.onload != null ) {
-        this.onload( { 'target' : this } );
+
+        //fire the load event
+        this.onload( {
+            target:           this,
+            lengthComputable: true,
+            loaded:           this.status.bytes_sent,
+            total:            this.status.bytes_total
+        } );
     }
+
 };
---
+++ development/uploader.php
@@ -0,0 +1,86 @@
+<?php
+
+if( $_SERVER[ 'REQUEST_METHOD' ] != 'POST' ) {
+    header( 'HTTP/1.1 405 Method Not Allowed', true, 405 );
+    header( 'Allow: POST' );
+    echo '<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"><title>Error 405: Method Not Allowed</title>
+</head>
+<body>
+  <h1>Error 405: Method Not Allowed</h1>
+</body>
+</html>';
+    exit();
+}
+
+if( isset( $_GET[ 'name' ] ) == false ) {
+    header( 'HTTP/1.1 400 Bad Request', true, 400 );
+    echo '<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"><title>Error 400: Bad Request</title>
+</head>
+<body>
+  <h1>Error 400: Bad Request</h1>
+</body>
+</html>';
+    exit();
+}
+
+//file meta data
+$info = [];
+
+//list of integer fields
+$int_fields = [ 'time_ms', 'size', 'offset', 'length' ];
+
+//load numeric fields
+for( $i = 0; $i < count( $int_fields ); ++$i ) {
+    if( isset( $_GET[ $int_fields[ $i ] ] ) == true ) {
+        $info[ $int_fields[ $i ] ] = intval( $_GET[ $int_fields[ $i ] ] );
+    }
+    else {
+        $info[ $int_fields[ $i ] ] = 0;
+    }
+}
+
+//strip any path information off of the file name
+$info[ 'name' ] = basename( $_GET[ 'name' ] );
+
+//check file modified timestamp
+if( isset( $info[ 'time_ms' ] ) == false ) {
+    date_default_timezone_set( 'UTC' );
+    $info[ 'time_ms' ] = time() * 1000;
+}
+
+//destination directory needs to be provided by application
+$dir = 'uploader';
+
+//write the POST contents to the file
+$ifh = fopen( 'php://input', 'rb' );
+if( $info[ 'offset' ] == 0 ) {
+    $mode = 'wb';
+}
+else {
+    $mode = 'ab';
+}
+$ofh = fopen( $dir . '/' . $info[ 'name' ], $mode );
+$size = 0;
+while( feof( $ifh ) == false ) {
+    $size += fwrite( $ofh, fread( $ifh, 8192 ) );
+}
+fclose( $ofh );
+fclose( $ifh );
+
+//store the file meta data
+file_put_contents(
+    $dir . '/' . $info[ 'name' ] . '.json',
+    json_encode( $info, JSON_PRETTY_PRINT )
+);
+
+//respond to the client
+header( 'Content-Type: application/json' );
+echo json_encode( $info );
+
+?>
