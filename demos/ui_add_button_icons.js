67a707f8f06f1236b9349ff6427db3357e0c5597
Zac Hester <zac.hester@gmail.com>
2014-04-21 14:45:43 -0500

    Adding names of new icons.

--- demos/ui_add_button_icons.js
+++ demos/ui_add_button_icons.js
@@ -3,9 +3,10 @@ function main( argv ) {
 
     var buttons = [
         'arrow_down', 'arrow_left', 'arrow_right', 'arrow_up',
-        'check', 'check_fill', 'cog', 'denied', 'document',
+        'check', 'check_fill', 'chevron_down', 'chevron_left',
+        'chevron_right', 'chevron_up', 'cog', 'denied', 'document',
         'document_filled', 'download', 'folder', 'info',
-        'info_fill', 'minus', 'plus', 'question_fill', 'save',
+        'info_fill', 'minus', 'pencil', 'plus', 'question_fill', 'save',
         'upload', 'x', 'x_fill'
     ];
 
