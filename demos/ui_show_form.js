26c7cb868c8aa131f9dae56a78dd48f0b6780b68
Zac Hester <zac.hester@gmail.com>
2014-04-23 19:30:48 -0500

    Updating ui.show_form demo to support current interface.

--- demos/ui_show_form.js
+++ demos/ui_show_form.js
@@ -1,9 +1,8 @@
 
 function main( argv ) {
-    p = hz.util.append();
+    var p = hz.util.append();
 
-    hz.ui.add.button(
-        p,
+    var button = hz.ui.create.button(
         'Show Form',
         function( event ) {
             hz.ui.show_form(
@@ -15,14 +14,27 @@ function main( argv ) {
                     'Notes' : { 'type' : 'edit' },
                     'Shhhh' : { 'type' : 'hidden', 'value' : 42 }
                 },
-                function( data ) {
+                function( event ) {
+                    var form = event.target;
+                    var data = form.getData();
                     console.log( JSON.stringify( data ) );
-                    //return true to close the form, false to keep it
+                    if( data.Name.length == 0 ) {
+                        hz.ui.error(
+                            'You must specify a name.',
+                            function( click ) {
+                                form.elements.Name.focus();
+                            }
+                        );
+                        return false;
+                    }
                     return true;
                 },
-                'To fill this form out, you must blah blah...'
+                'To fill this form out, you must blah blah...',
+                'Submit Me!'
             );
         }
     );
+
+    p.appendChild( button );
 
 }
