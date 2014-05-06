7f682ad7cdf327bf61417cfd30ec9f47a18bbd8d
Zac Hester <zac.hester@gmail.com>
2014-05-06 10:06:12 -0500

    Adding minor DOM extensions.

--- demos/html.js
+++ demos/html.js
@@ -22,6 +22,7 @@ function main( argv ) {
     obj.a2.href = 'http://example.com/doc3.html';
     obj.im.src  = '../../hzphp/tools/icon.php?id=check_fill';
 
+    //testing HTMLElement.getURLNodes() method
     var button = hz.ui.create.button(
         'Iterate URLers',
         function( event ) {
@@ -36,6 +37,7 @@ function main( argv ) {
     );
     hz.util.append( null, button );
 
+    //
     button = hz.ui.create.button(
         'Prefix Relative URLs',
         function( event ) {
@@ -49,5 +51,42 @@ function main( argv ) {
         }
     );
     hz.util.append( null, button );
+
+    //testing textValue accessor property
+    var p1 = hz.util.append();
+    var ds1 = new hz.doc.structure( 'span,_text_,input', [ 'a', 'b', 'c' ] );
+    button = hz.ui.create.button(
+        'HTMLElement.setText()',
+        function( event ) {
+            console.log( 'span:   ' + ds1.get_node( 0 ).textValue );
+            console.log( '_text_: ' + ds1.get_node( 1 ).textValue );
+            console.log( 'input:  ' + ds1.get_node( 2 ).textValue );
+            ds1.get_node( 0 ).textValue = 'd';
+            ds1.get_node( 1 ).textValue = 'e';
+            ds1.get_node( 2 ).textValue = 'f';
+        }
+    );
+    hz.util.append( p1, button );
+    hz.util.append( p1, ds1 );
+
+    //testing HTMLElement.bindElements()
+    var p2 = hz.util.append();
+    var ds2 = new hz.doc.structure( 'span,_text_,span', [ 'a', 'b', 'c' ] );
+    ds2.get_node( 0 ).id = 'id_a';
+    ds2.get_node( 1 ).id = 'id.b';
+    ds2.get_node( 2 ).id = 'id-c';
+    button = hz.ui.create.button(
+        'HTMLElement.bindElements()',
+        function( event ) {
+            p2.bindElements();
+            for( var prop in p2 ) {
+                if( prop.substring( 0, 2 ) == 'id' ) {
+                    console.log( prop + ': ' + p2[ prop ].nodeType );
+                }
+            }
+        }
+    );
+    hz.util.append( p2, button );
+    hz.util.append( p2, ds2 );
 
 }
